fa1c34a5377e7f1d62ea4029ade1f0118f8915d6
niyazahmad790 <niyazahmad7071@gmail.com>
2026-06-12 10:47:33 +0530

    School Management System - OOPs complete

---
+++ src/SchoolManagement/Teacher.java
@@ -0,0 +1,33 @@
+package SchoolManagement;
+
+public class Teacher extends Person {
+
+private int salary;
+private String subject;
+
+ public void setSalary(int salary){
+     this.salary = salary;
+
+ }
+ public void setSubject(String subject){
+     this.subject = subject;
+ }
+
+ public int getSalary(){
+     return salary;
+
+ }
+    public String getSubject(){
+     return subject;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Teacher Name " + getName());
+        System.out.println("Salary " + getSalary());
+        System.out.println("Subject " + getSubject());
+    }
+}
+
+
+
